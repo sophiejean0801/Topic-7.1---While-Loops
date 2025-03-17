ca848e0088cb45d17c8be9b784994af675087ba8
Sophie Jean <363449299@GoTVDSB.ca>
2025-03-17 14:34:29 -0400

    i hate loops so much oh my god

--- Topic 7.1 - While Loops/Program.cs
+++ Topic 7.1 - While Loops/Program.cs
@@ -4,7 +4,51 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int pin = 12345, tries = 0;
+            int entry, age;
+            bool done = false;
+            
+            Console.WriteLine("WELCOME TO THE BANK OF JEAN");
+            while (!done) {
+                Console.WriteLine("ENTER YOUR PIN: ");
+                while (!int.TryParse(Console.ReadLine(), out entry)) ;
+                tries++;
+                if (entry == pin)
+                {
+                    Console.WriteLine("\nPIN ACCEPTED. YOU NOW HAVE ACCESS TO YOUR ACCOUNT.");
+                    done = true;
+                }
+                else if (tries == 5)
+                {
+                    Console.WriteLine("\nYOU HAVE RUN OUT OF TRIES. ACCOUNT LOCKED.");
+                    done = true;
+                }
+                else
+                {
+                    Console.WriteLine("\nINCORRECT PIN. TRY AGAIN.");
+                }
+            while (!done)
+            {
+                Console.WriteLine("Enter your age: ");
+                int.TryParse(Console.ReadLine(), out age);
+                if (age < 0)
+                {
+                    Console.WriteLine("I can tell you're still in the womb. Try again.");
+                    continue;
+                }
+                else
+                {
+                    done = true;
+
+                }
+            } 
+
+
+
         }
+
+
+    }
     }
 }
+
