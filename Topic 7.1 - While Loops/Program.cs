fc4ebacf465a501b48120b50f3163ab1348a816d
Sophie Jean <363449299@GoTVDSB.ca>
2025-03-18 14:05:37 -0400

    loops suck

--- Topic 7.1 - While Loops/Program.cs
+++ Topic 7.1 - While Loops/Program.cs
@@ -7,9 +7,9 @@
             int pin = 12345, tries = 0;
             int entry, age;
             bool done = false;
-            
             Console.WriteLine("WELCOME TO THE BANK OF JEAN");
-            while (!done) {
+            while (!done)
+            {
                 Console.WriteLine("ENTER YOUR PIN: ");
                 while (!int.TryParse(Console.ReadLine(), out entry)) ;
                 tries++;
@@ -26,29 +26,60 @@
                 else
                 {
                     Console.WriteLine("\nINCORRECT PIN. TRY AGAIN.");
+                    tries++;
                 }
-            while (!done)
-            {
-                Console.WriteLine("Enter your age: ");
-                int.TryParse(Console.ReadLine(), out age);
-                if (age < 0)
+
                 {
-                    Console.WriteLine("I can tell you're still in the womb. Try again.");
-                    continue;
+                    Console.WriteLine("Enter your age: ");
+                    int.TryParse(Console.ReadLine(), out age);
+                    if (age < 0)
+                    {
+                        Console.WriteLine("I can tell you're still in the womb. Try again.");
+                        continue;
+                    }
+                    else
+                    {
+                        done = true;
+
+                    }
                 }
-                else
+                int count = 0; // Will count number of times we have looped
+                Console.WriteLine("Type out a message and I will repeat it five times");
+                Console.Write("Message: ");
+                string message = Console.ReadLine();
+                while (count < 5)
                 {
-                    done = true;
+                    Console.WriteLine((count + 1) + ". " + message);
+                    count++; //Adds 1 to our counter
+                }
+                Console.ForegroundColor = ConsoleColor.Red;
+                int count2= 0; // Will count number of times we have looped
+                Console.WriteLine("Type out a message and I will repeat it");
+                Console.Write("Message: ");
+                string message2= Console.ReadLine();
+                Console.Write("How many times would you like me to repeat it? ");
+                int repeat = int.Parse(Console.ReadLine());
+                while (count2< repeat)
+                {
+                    Console.WriteLine((count2 + 1) + ". " + message2);
+                    count2++; //Adds to our counter
+                }
+                int total = 0;
+                int count3 = 0;
+                Console.WriteLine("I will add up the numbers you give me.");
+                Console.WriteLine("How many numbers would you like to add?");
+                int numbers = int.Parse(Console.ReadLine());
+                while (numbers > 0)
+                {
+                    int number = int.Parse(Console.ReadLine());
+                    total += number;
 
                 }
-            } 
 
 
 
+
+            }
         }
-
-
-    }
     }
 }
-
